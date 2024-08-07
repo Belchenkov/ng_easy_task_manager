7f1e6c9898042f3f4a62c93090b498c5b1fd2034
Belchenkov <u608110@gmail.com>
2024-08-07 11:30:46 +0400

    Create task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
@@ -12,16 +13,21 @@ import { DUMMY_USERS } from "./dummy-users";
     RouterOutlet,
     HeaderComponent,
     UserComponent,
+    TasksComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Easy Task Manager';
-
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string): void {
-
+    this.selectedUserId = id;
   }
 }
