ebac833e21ab3209824a1bd11cd23e56e21d66ec
Belchenkov <u608110@gmail.com>
2024-08-14 16:03:03 +0400

    User Interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 
-import { DUMMY_USERS } from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+interface IUser {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: "app-user",
@@ -11,16 +13,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: "./user.component.css"
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: IUser;
   @Output() select = new EventEmitter<string>();
 
   get imagePath(): string {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser(): void {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
