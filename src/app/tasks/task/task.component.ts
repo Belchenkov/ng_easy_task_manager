7c4a55940db21672737daf2ba79e44ada07bd6c3
Belchenkov <u608110@gmail.com>
2024-08-28 12:08:12 +0400

    Split on models

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,6 @@
 import { Component, Input } from "@angular/core";
 
-interface ITask {
-  id: string;
-  userId: string;
-  title: string;
-  summary: string;
-  dueDate: string;
-}
+import { type ITask } from "./task.model";
 
 @Component({
   selector: 'app-task',
