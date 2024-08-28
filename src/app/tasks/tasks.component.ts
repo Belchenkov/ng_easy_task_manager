4f474ab7ef7d8cfeb49510901e10c93aec257cc6
Belchenkov <u608110@gmail.com>
2024-08-28 11:52:04 +0400

    Output user tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -12,6 +12,33 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({ required: true }) name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
 
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master React',
+      summary: 'Learn all the basic and advanced features of React & how to apply them.',
+      dueDate: '2025-10-31',
+    },{
+      id: 't3',
+      userId: 'u3',
+      title: 'Master Vue',
+      summary: 'Learn all the basic and advanced features of Vue & how to apply them.',
+      dueDate: '2025-11-31',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
